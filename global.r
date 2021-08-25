63a821e7465fe11374e040b7c525d7b46d479363
Your NameClaudeGrasland <claude.grasland@parisgeo.cnrs.fr>
2021-08-25 18:08:29 +0200

    correction of models & TELEMAC

--- global.r
+++ global.r
@@ -2,13 +2,13 @@
 #setwd("/data/user/g/cgrasland/frontier/hypernews")
 
 # Load functions
-source("pgm/hypernews_functions_V4.R")
+source("pgm/hypernews_functions_V5.R")
 
 # Load hypercube(s)
 dts1<-readRDS("data/hc_EUR_foreign_border_week.Rdata")
 dts2<-readRDS("data/hc_EUR_foreign_mobil_week.Rdata")
-#dts3<-readRDS("hc_EUR_foreign_pandemic_week.Rdata")
-dts<-list("border"=dts1,"mobil"=dts2)
+dts3<-readRDS("data/hc_EUR_foreign_pandemic_week.Rdata")
+dts<-list("border"=dts1,"mobil"=dts2, "pande"=dts3)
 
 
 
